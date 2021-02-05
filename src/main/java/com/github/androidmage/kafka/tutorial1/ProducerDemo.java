c9b7b26ce35e8e7cb48ad3a0dfd816fa3d3f02b0
Allen Hu <allensamuelhu@gmail.com>
2021-02-05 15:41:30 -0500

    Before producer demo

---
+++ src/main/java/com/github/androidmage/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,15 @@
+package com.github.androidmage.kafka.tutorial1;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        // create Producer properties
+
+
+        // create the producer
+
+
+        //send data
+    }
+}
